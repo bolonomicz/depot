dfa395f5114413a3238a4dc77fdc5ffe1780939b
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-28 21:53:19 -0400

    Depot scaffold, Task A

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create(title: 'Iphone',
+  description: 
+    %{<p>
+       Apple Product
+      </p>},
+  image_url:   'iphone.jpg',    
+  price: 536.00)
+# . . .
+Product.create(title: 'Galaxy S4',
+  description:
+    %{<p>
+     		Samsung smartphone, android
+      </p>},
+  image_url: 'galaxy.jpg',
+  price: 540.12)
+# . . .
+
+Product.create(title: 'z10',
+  description: 
+    %{<p>
+        Blackberry product , bb10 os
+      </p>},
+  image_url: 'z10.jpg',
+  price: 234.23)
