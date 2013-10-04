9d85aa96c92645151da16c429eca9e54e3dc1d28
Ralph Bolo <ralph.bolo@gmail.com>
2013-10-04 12:32:45 -0400

    finding a cart D1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private 
+
+  	def current_car
+  		Cart.find(session[:cart_id])
+  	rescue ActiveRecord::RecordtNotFound
+  		cart = Cart.create
+  		session[:cart_id] = cart.id
+  	end
 end
